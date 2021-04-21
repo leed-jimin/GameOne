9389c2b7d7b1e8975ca269adadf2773dea302106
Daniel Lee <leed.jimin@gmail.com>
2021-04-21 00:46:41 -0700

    added files

---
+++ GoDot/itIsAGame/characterAssets/scripts/character/AnimationHandler.gd
@@ -0,0 +1,107 @@
+class_name AnimationHandler
+
+var anim
+var tree
+var playerDetails
+var timer
+
+#these will be loaded from playerdetails
+var lightAttkPoints = 0
+var heavyAttkPoints = 0
+
+var lightWaitTime = .7
+var heavyWaitTime = 1
+
+var lightAttkArr = ["lJab", "rJab", "r_l_f_straightPunch"] #will be loaded from details later
+var heavyAttkArr = ["lFrontKick"]
+var lightAirArr = ["r_l_aerialElbow"] #will be loaded from details later
+var heavyAirkArr = []
+#these will be loaded from playerdetails
+
+
+func _init(animPlayer, animTree, playerDet, timerNode):
+	playerDetails = playerDet
+	tree = animTree
+	anim = animPlayer
+	timer = timerNode
+	tree.active = true
+	tree.set("parameters/Ground_Move/current", 3)
+
+func _ready():
+	anim.get_animation("Walking").set_loop(true)
+	anim.get_animation("Idle").set_loop(true)
+	anim.get_animation("Running").set_loop(true)
+
+func handle_aerial_movement_animation(just_jumped, grounded, moveVec):
+	if just_jumped:
+		tree.set("parameters/Air_Move/current", 0)
+		tree.set("parameters/state/current", 1)
+		playerDetails.geoState.set_currState("AIR")
+	elif tree.get("parameters/state/current") == 1 and grounded:
+		tree.active = false
+		tree.set("parameters/state/current", 0)
+		play_anim("FallDown")
+	elif grounded:
+		tree.active = true
+		playerDetails.geoState.set_currState("GROUND")
+		tree.set("parameters/state/current", 0)
+		if moveVec.x == 0 and moveVec.z == 0:
+			print("walk")
+			tree.set("parameters/Ground_Move/current", 3)
+		else:
+			tree.set("parameters/Ground_Move/current", 1)
+	
+func handle_attack_animation(type):
+	playerDetails.actionState.set_currState("BUSY")
+	tree.active = false
+	if type == "light_attack":
+		if playerDetails.geoState.get_currState() == playerDetails.geoState.get_states()["AIR"]:
+			play_anim(lightAirArr[0])
+		else:
+			lightAttkPoints = lightAttkPoints % (lightAttkArr.size())
+			timer.wait_time = lightWaitTime
+			timer.start()
+			if lightAttkPoints != lightAttkArr.size():
+				anim.queue(lightAttkArr[lightAttkPoints])
+			lightAttkPoints = lightAttkPoints + 1
+	elif type == "heavy_attack":
+		if playerDetails.geoState.get_currState() == playerDetails.geoState.get_states()["AIR"]:
+			pass
+		else:
+			heavyAttkPoints = heavyAttkPoints % (heavyAttkArr.size())
+			timer.wait_time = heavyWaitTime
+			timer.start()
+			if heavyAttkPoints != heavyAttkArr.size():
+				anim.queue(heavyAttkArr[heavyAttkPoints])
+			heavyAttkPoints = heavyAttkPoints + 1
+	
+	#playerDetails.set_currCharState(charStates["IDLE"])
+	
+func handle_block_animation():
+	pass
+	
+func handle_specials_animation():
+	pass
+	
+func handle_knockback_animation():
+	anim.clear_queue()
+	pass
+
+func timer_timeout():
+	lightAttkPoints = 0
+	heavyAttkPoints = 0
+	timer.stop()
+	
+func play_anim(name):
+	if anim.current_animation != null and anim.current_animation == name:
+		return
+	anim.play(name)
+	
+func set_to_idle():
+	print("here")
+	tree.set("parameters/state/current", 0)
+	tree.set("parameters/Ground_Move/current", 3)
+	tree.active = true
+	playerDetails.actionState.set_currState("NONE")
+	playerDetails.geoState.set_currState("GROUND")
+	
---
+++ GoDot/itIsAGame/characterAssets/scripts/character/MainMovement.gd
@@ -0,0 +1,92 @@
+extends KinematicBody
+
+var Player_Details = load("res://characterAssets/scripts/character/PlayerDetails.gd") # Relative path
+onready var playerDetails = Player_Details.new()
+
+var AnimHandler = load("res://characterAssets/scripts/character/AnimationHandler.gd")
+onready var animationHandler = AnimHandler.new($AnimationPlayer, $AnimationTree, playerDetails, $Timer)
+
+const SPEED_WALK = 15
+const SPEED_RUN = 30
+const JUMP_FORCE = 60
+const GRAVITY = 1.3
+#const SPEED_MAX_FALL = 40
+
+var yVelo = 0
+var airbourne = false
+
+onready var characterModel = $characterModel
+
+func _ready():
+	pass
+	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _physics_process(_delta):
+	var moveVec = Vector3()
+	#Directional movement
+	if Input.is_action_pressed("ui_left"):
+		if Input.is_action_pressed("ui_up"):
+			rotation_degrees = Vector3(0, -135, 0)
+		elif Input.is_action_pressed("ui_down"):
+			rotation_degrees = Vector3(0, -45, 0)
+		else:
+			rotation_degrees = Vector3(0, -90, 0)
+		moveVec.z += 1
+	elif Input.is_action_pressed("ui_right"):
+		if Input.is_action_pressed("ui_up"):
+			rotation_degrees = Vector3(0, 135, 0)
+		elif Input.is_action_pressed("ui_down"):
+			rotation_degrees = Vector3(0, 45, 0)
+		else:
+			rotation_degrees = Vector3(0, 90, 0)
+		moveVec.z += 1
+	elif Input.is_action_pressed("ui_up"):
+		rotation_degrees = Vector3(0, 180, 0)
+		moveVec.z += 1
+	elif Input.is_action_pressed("ui_down"):
+		rotation_degrees = Vector3(0, 0, 0)
+		moveVec.z += 1
+	#Directional movement End
+	
+	#Jumping logic
+	var grounded = is_on_floor()
+	var just_jumped = false
+	
+	if grounded:
+		if Input.is_action_just_pressed("jump"):
+			just_jumped = true
+			yVelo = JUMP_FORCE
+	else:
+		playerDetails.geoState.set_currState("AIR")
+		yVelo -= GRAVITY
+		
+	#print(playerDetails.get_currCharState())
+	if playerDetails.actionState.get_currState() == playerDetails.actionState.get_states()["NONE"] || playerDetails.geoState.get_currState() == playerDetails.geoState.get_states()["AIR"]:
+		move_and_slide_wrapper(moveVec)
+		animationHandler.handle_aerial_movement_animation(just_jumped, grounded, moveVec)
+	#Jumping logic End
+	
+	#Standard Attacks
+	if Input.is_action_just_pressed("light_attack"):
+		animationHandler.handle_attack_animation("light_attack")
+	if Input.is_action_just_pressed("heavy_attack"):
+		animationHandler.handle_attack_animation("heavy_attack")
+	#Standard Attacks End
+	
+func move_and_slide_wrapper(moveVec):
+	var currSpeed = SPEED_WALK
+	if playerDetails.geoState.get_currState() == playerDetails.geoState.get_states()["AIR"]:
+		currSpeed = SPEED_WALK / 2
+	moveVec = moveVec.normalized()
+	moveVec = moveVec.rotated(Vector3(0, 1, 0), rotation.y)
+	moveVec *= currSpeed
+	moveVec.y = yVelo
+	move_and_slide(moveVec, Vector3(0, 1, 0))
+
+
+func _on_AnimationPlayer_animation_finished():
+	animationHandler.set_to_idle()
+
+
+func _on_Timer_timeout():
+	animationHandler.timer_timeout() # Replace with function body.
